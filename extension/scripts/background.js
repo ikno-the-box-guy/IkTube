7b16ff51fe9b9be17f1ea55caaf6efa8ab48c8e1
ikno-the-box-guy <erwtje013@gmail.com>
2025-01-25 23:43:44 +0100

    Added basic extension frontend

---
+++ extension/scripts/background.js
@@ -0,0 +1,69 @@
+﻿// TODO: Download in the background, so you can close the popup or go to a different tab while its working
+
+let downloadingAudio = false;
+
+function downloadAudio(videoId) {
+    downloadingAudio = true; 
+    
+    chrome.downloads.download({
+        url: `http://localhost:3000/api/v1/audio/${videoId}`,
+        saveAs: false
+    }, (downloadId) => {
+        chrome.runtime.sendMessage({
+            type: 'status',
+            format: 'audio',
+            downloading: false
+        })
+        
+        downloadingAudio = false;
+        
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        }
+    });
+}
+
+let downloadingVideo = false;
+
+function downloadVideo(videoId) {
+    downloadingVideo = true;
+
+    chrome.downloads.download({
+        url: `http://localhost:3000/api/v1/video/${videoId}`,
+        saveAs: false
+    }, (downloadId) => {
+        chrome.runtime.sendMessage({
+            type: 'status',
+            format: 'video',
+            downloading: false
+        })
+
+        downloadingVideo = false;
+
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        }
+    });
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'download') {
+        if (message.format === 'audio') {
+            downloadAudio(message.videoId);
+        }
+        
+        if (message.format === 'video') {
+            downloadVideo(message.videoId);
+        }
+    }
+    
+    if (message.type === 'status') {
+        if (message.format === 'audio') {
+            sendResponse({downloading: downloadingAudio});
+        }
+
+        if (message.format === 'video') {
+            sendResponse({downloading: downloadingVideo});
+        }
+    }
+});
---
+++ extension/scripts/downloader.js
@@ -0,0 +1,87 @@
+﻿let videoId = undefined;
+
+// Get the download video button
+const downloadVideoButton = document.getElementById("download-video");
+downloadVideoButton.addEventListener("click", downloadVideo);
+
+// Get the download audio button
+const downloadAudioButton = document.getElementById("download-audio");
+downloadAudioButton.addEventListener("click", downloadAudio);
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    const activeTab = tabs[0];
+    const url = activeTab.url;
+
+    const regex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu\.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]{11})((?:\?|\&)\S+)?$/;
+
+    const match = url.match(regex);
+    
+    if (match) {
+        videoId = match[6];
+        
+        downloadVideoButton.disabled = false;
+        downloadAudioButton.disabled = false;
+    }
+});
+
+chrome.runtime.sendMessage({
+    type: 'status',
+    format: 'audio',
+}).then((response) => {
+    if (response.downloading && videoId) {
+        downloadAudioButton.disabled = true;
+        downloadAudioButton.setAttribute('downloading', '');
+    }
+})
+
+chrome.runtime.sendMessage({
+    type: 'status',
+    format: 'video',
+}).then((response) => {
+    if (response.downloading && videoId) {
+        downloadVideoButton.disabled = true;
+        downloadVideoButton.setAttribute('downloading', '');
+    }
+})
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'status') {
+        if (message.format === 'audio') {
+            if (!message.downloading && videoId) {
+                downloadAudioButton.disabled = false;
+                downloadAudioButton.removeAttribute('downloading');
+            }
+        }
+
+        if (message.format === 'video') {
+            if (!message.downloading && videoId) {
+                downloadVideoButton.disabled = false;
+                downloadVideoButton.removeAttribute('downloading');
+            }
+        }
+    }
+});
+
+function downloadVideo() {
+    downloadVideoButton.disabled = true;
+    downloadVideoButton.setAttribute('downloading', '');
+
+    chrome.runtime.sendMessage({
+        type: 'download',
+        format: 'video',
+        videoId: videoId
+    });
+}
+
+function downloadAudio() {
+    downloadAudioButton.disabled = true;
+    downloadAudioButton.setAttribute('downloading', '');
+    
+    chrome.runtime.sendMessage({
+        type: 'download',
+        format: 'audio',
+        videoId: videoId
+    });
+}
