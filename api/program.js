75de658c5e8735b72c644f5b0eb3b14736c7284c
ikno-the-box-guy <erwtje013@gmail.com>
2025-01-25 01:54:02 +0100

    Fixed API issues

--- api/program.js
+++ api/program.js
@@ -7,6 +7,13 @@ execSync("yt-dlp -U", {stdio: "inherit"});
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use('/api/v1/', router)
 app.listen(3000, () => {
     console.log('\nServer is running on port 3000')
--- api/router.js
+++ api/router.js
@@ -4,7 +4,7 @@ import {downloadAudio, downloadVideo} from "./controller.js";
 const router = express.Router();
 
 // TODO: Add thumbnail download
-router.get("/video/", downloadVideo);
-router.get("/audio/", downloadAudio);
+router.post("/video/", downloadVideo);
+router.post("/audio/", downloadAudio);
 
 export default router;
